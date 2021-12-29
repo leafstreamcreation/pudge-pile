5180108d6598b6aaa09872360266c501c8cda7cf
Derek <shouldergiant@icloud.com>
2021-12-29 16:12:51 -0500

    Add schedule date to regenTask for regen sync

--- models/RegeneratingTask.model.js
+++ models/RegeneratingTask.model.js
@@ -4,6 +4,7 @@ const { Schema, model } = require("mongoose");
 const regenTaskSchema = new Schema({
   text: { type: String, required: true },
   regenInterval: { type: Number, required: true },
+  scheduleDate: { type: Date, required: true },
 }, {
   timestamps: true,
 });
